bae4657db7e008eef520a19545ba6e23533ecd66
arnold1945 <vscj58@gmail.com>
2025-11-01 16:14:45 -0500

    practice

--- main.py
+++ main.py
@@ -4,55 +4,67 @@ def match_alpha_only(s: str) -> bool:
     """
     Match strings that contain only alphabetic characters (uppercase or lowercase).
     """
-    pass
+    
+    result = bool(re.match(r'^[a-zA-Z]+$', s))
+    return result
+    
 
 def match_all_lowercase(s: str) -> bool:
     """
     Match strings that contain only lowercase letters.
     """
-    pass
+    result = bool(re.match(r'^[a-z]+$', s))
+    print(result)
+    return result
 
 def match_all_uppercase(s: str) -> bool:
     """
     Match strings that contain only uppercase letters.
     """
-    pass
+    result = bool(re.match(r'^[A-Z]+$',s))
+    return result
 
 def match_email_format(s: str) -> bool:
     """
     Match a valid email address format (e.g., example@domain.com).
     """
-    pass
+    result = bool(re.match(r'^[\w\.-]+@[\w\.-]+\w\.com$', s))
+    return result
 
 def match_us_phone(s: str) -> bool:
     """
     Match a US phone number format: 123-456-7890
     """
-    pass
+    result = bool(re.match(r'^(\d{3})-(\d{3})-(\d{4})',s))
+    return result
 
 def match_date_mmddyyyy(s: str) -> bool:
     """
     Match a date in the format MM/DD/YYYY.
     """
-    pass
+    result = bool(re.match(r'^(0[1-9]|1[1-2])/([0-2][0-9]|3[0-1])/(\d{4})', s))
+    return result
 
 def match_postal_code(s: str) -> bool:
     """
     Match a 5-digit US ZIP code.
     """
-    pass
+    result = bool(re.match(r'^(\d{5}$)',s))
+    return result
 
 def match_hex_color(s: str) -> bool:
     """
     Match a hex color code (e.g., #FFF or #FFFFFF).
     """
-    pass
+    result=bool(re.match(r'^#(\w+|W+)$', s))
+    return result
 
 def match_time_24h(s: str) -> bool:
     """
     Match a time in 24-hour format (e.g., 14:30).
     """
-    pass
+    result = bool(re.match(r'^(0[1-9]|1[0-9]|2[0-3]):([0-5][0-9])',s))
+    return result
 
 def match_valid_username(s: str) -> bool:
     """
